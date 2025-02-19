6fdbca3c69709771b2390fcf5ce9f56dc405cb2b
marie <marie4503@naver.com>
2025-02-19 23:43:46 +0900

    add build file

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,9 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 export default defineConfig({
   plugins: [vue(), vueDevTools()],
   base: '/Vue_study/',
+  build: {
+    outDir: 'docs',
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
