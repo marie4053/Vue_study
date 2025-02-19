ca0a88c0c34ee608ae4d2fef99dc8a6c7e7be9cc
marie <marie4503@naver.com>
2025-02-19 21:22:15 +0900

    feat: vuetify button test

--- src/views/VuefityTest.vue
+++ src/views/VuefityTest.vue
@@ -2,6 +2,16 @@
 
 <template>
   <div>
-    <v-btn density="comfortable">Primary 버튼</v-btn>
+    <v-container width="400px">
+      <v-row justify="center" align="center">
+        <v-col cols="auto">
+          <v-btn>Primary 버튼</v-btn>
+        </v-col>
+        <v-spacer />
+        <v-col cols="auto">
+          <v-btn variant="tonal" class="bg-red">tonal 버튼</v-btn>
+        </v-col>
+      </v-row>
+    </v-container>
   </div>
 </template>
